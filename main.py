a4032158210116646e4a4e72401c61d5e8158df4
Tolgonai022002 <104982690+Tolgonai022002@users.noreply.github.com>
2022-11-14 18:53:47 +0600

    added main

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+def new_game():
+    pass
+def check_answer():
+    pass
+def display_score():
+    pass
+def play_again():
+    pass
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main():
+    pass
